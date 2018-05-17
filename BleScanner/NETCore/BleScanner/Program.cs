566d16082410448693d13bc7e9268d7e30731985
Casey Anderson <caseya@bluecats.com>
2018-05-17 17:34:21 -0500

    A bit of cleanup and some bug fixes

--- BleScanner/NETCore/BleScanner/Program.cs
+++ BleScanner/NETCore/BleScanner/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
+using System.IO;
 using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using BlueCats.Ble.Serial.BC0xx;
+using BlueCats.Ble.Serial.BC0xx.Commands;
 using BlueCats.Ble.Serial.BC0xx.Commands.Base;
 using BlueCats.Ble.Serial.BC0xx.Events;
 using BlueCats.Ble.Serial.BC0xx.Events.Base;
@@ -13,31 +18,46 @@ namespace BleScanner {
 
     class Program {
 
+        // Serial properties
         static int Baud = 921600;
         static string PortName;
         static SerialPort Serial;
-        static SerialProtocol Protocol;
-        static string ProtocolError;
-        static EventPdu EventPdu;
-        static CommandResponsePdu ResponsePdu;
-        static byte[] SerialBuffer = new byte[ 4096 ]; // FIFO
+        static readonly byte[] SerialBuffer = new byte[ 4096 ]; // FIFO
         static int SerialBufferTail; // Data start index (inclusive)
         static int SerialBufferHead; // Data end index (exclusive)
         static int SerialBufferCount => SerialBufferHead - SerialBufferTail;
 
+        // SerialProtocol properties
+        static SerialProtocol Protocol;
+        static string ProtocolError;
+        static EventPdu EventPdu;
+        static CommandResponsePdu ResponsePdu;
+
+        // Loop Location Engine properties
+        const int LOOP_PORT = 19111; // Example Loop port
+        const string LOOP_HOST = "127.0.0.1"; // Example Loop IP
+        static IPEndPoint Endpoint;
+        static Socket LoopSocket;
+        static byte SequenceNumber;
+        static bool LoopForwardingEnabled;
+
+        // BLE Scanner properties
+        static byte[] ScannerMacAddress;
+
         static void Main( string[] args ) {
             try {
-                // Handle args
-                if ( ( args.Length < 1 ) || 
-                    args[ 0 ].Equals( "-h" ) || 
-                    args[ 0 ].Equals( "--help" ) ) {
-                    PrintHelp();
-                    return;
-                }
-                ParseArgs( args );
+                // Parse args
+                var success = ParseArgs( args );
+                if ( !success ) return;
 
                 // Make console fast for large screens
                 Console.BufferHeight = 1024;
+
+                // Init UDP Client
+                if ( LoopForwardingEnabled ) {
+                    LoopSocket = new Socket( AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp );
+                    Endpoint = new IPEndPoint( IPAddress.Parse( LOOP_HOST ), LOOP_PORT );
+                }
 
                 // Init serial port
                 Serial = new SerialPort( PortName, Baud );
@@ -53,9 +73,18 @@ namespace BleScanner {
 
                 // Send StopScan command in-case it was left in a scanning state 
                 WriteCommand( SerialProtocol.CreateStopScanCommand() );
-                ReadResponse(); // Not checking the result because this is just a reset attempt
+                ReadResponse(); // Not checking result because this reset is optional (ensuring good starting point)
 
-                // *Asynchronously* start scanning for BLE devivces
+                // Read MAC Address of USB BLE Scanner
+                if ( LoopForwardingEnabled ) {
+                    WriteCommand( SerialProtocol.CreateReadBluetoothAddressCommand() );
+                    var response = ReadResponse();
+                    if ( response.ResponseCode != CommandResponseCode.Ack ) {
+                        throw new Exception( $"Received response code: {response.ResponseCode}" );
+                    }
+                    ScannerMacAddress = response.As< ReadBluetoothAddressCommandResponse >()?.BluetoothAddress;
+                }
+                // *Asynchronously* start scanning for BLE devices
                 var scanCancellation = new CancellationTokenSource();
                 var cancelToken = scanCancellation.Token;
                 var scanTask = Task.Run( async () => {
@@ -82,7 +111,7 @@ namespace BleScanner {
                 }
 
                 // Rethrow any exceptions from our async tasks (except cancellation)
-                // Note: GetAwaiter().GetResult() unwraps AggregateExceptions before throwing
+                // Note: GetAwaiter().GetResult() unwraps AggregateExceptions
                 try { scanTask.GetAwaiter().GetResult(); }
                 catch ( OperationCanceledException ) {}
             }
@@ -94,49 +123,73 @@ namespace BleScanner {
             }
         }
 
-        static void OnEventReceived( EventPdu eventPdu ) {
-            if ( eventPdu.Header.EventCode != EventCode.DeviceDiscovered ) return;
-            var discoveredEvent = eventPdu.As< DeviceDiscoveredEvent >();
-            var mac = DataConverter.ByteArrayToHexString( discoveredEvent.BluetoothAddress, delimiter: ":" ); 
-            var rssi = discoveredEvent.RSSI;
-            var bleAdvRawData = DataConverter.ByteArrayToHexString( discoveredEvent.AdData, delimiter: " " );
-            Console.WriteLine( $"{mac}  {rssi:###} dBm  {bleAdvRawData}" );
-        }
-
-        static void WriteCommand( CommandPdu command ) {
-            var commandBytes = command.ToByteArray();
-            Serial.Write( commandBytes, 0, commandBytes.Length );
-        }
-
-        static Task StartEventPollingTask( CancellationToken cancelToken ) {
-            return Task.Run( () => {
-                while ( !cancelToken.IsCancellationRequested ) {
-                    var b = ReadSerialByteBuffered();
-                    Protocol.Parse( b );
-                    if ( CheckForProtocolError( out var error ) ) {
-                        PrintError( $"Protocol error: {error}" );
-                    }
-                    if ( CheckForEvent( out var eventPdu ) ) {
-                        OnEventReceived( eventPdu );
+        static bool ParseArgs( string[] args ) {
+            try {
+                if ( args[ 0 ].StartsWith( "-" ) ) throw new Exception();
+                PortName = args[ 0 ];
+                if ( args.Length > 1 ) {
+                    if ( args[ 1 ].Equals( "-l" ) ) {
+                        LoopForwardingEnabled = true;
                     }
                 }
-            }, cancelToken );
+                return true;
+            }
+            catch {
+                PrintHelp();
+                return false;
+            }
         }
 
-        static CommandResponsePdu ReadResponse() {
-            while ( true ) {
-                var rxByte = ReadSerialByteBuffered();
-                Protocol.Parse( rxByte );
-                if ( CheckForProtocolError( out var error ) ) {
-                    throw new Exception( $"Protocol error: {error}" );
+        static void PrintHelp() {
+            Console.WriteLine();
+            Console.WriteLine( "Usage:");
+            Console.WriteLine( "  dotnet BleScanner.dll <serial name> [ -h | -l ]" );
+            Console.WriteLine();
+            Console.WriteLine( "Example:");
+            Console.WriteLine( "  dotnet BleScanner.dll com3 -l" );
+            Console.WriteLine();
+            Console.WriteLine( "  -l                         Send to Loop Location Engine (UDP)" );
+            Console.WriteLine( "  -h, --help                 Print this message" );
+
+        }
+
+        static void PrintError( string message ) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine( message );
+            Console.ResetColor();
+        }
+
+        static void Cleanup() {
+            try {
+                // Send StopScan to reset the scanner state
+                if ( Serial != null ) { 
+                    WriteCommand( SerialProtocol.CreateStopScanCommand() );
                 }
-                if ( CheckForResponse( out var responsePdu ) ) {
-                    return responsePdu;
+            }
+            catch ( Exception ex ) {
+                PrintError( ex.Message );
+            }
+            try {
+                Serial?.Close();
+            }
+            catch ( Exception ex ) {
+                PrintError( ex.Message );
+            }
+        }
+
+        static void WaitForKeypress( CancellationToken cancelToken ) {
+            try {
+                var readKeyTask = Task.Run( () => Console.ReadKey( true ), cancelToken );
+                readKeyTask.Wait( cancelToken );
+            }
+            catch ( Exception ex ) {
+                if ( !( ex is OperationCanceledException ) ) {
+                    Debug.WriteLine( ex );
                 }
             }
         }
 
-        static byte ReadSerialByteBuffered() {
+        static byte ReadByteFromSerialBuffer() {
             // If serial buffer is empty, buffer up the next chunk of serial data
             if ( SerialBufferCount <= 0 ) {
                 SerialBufferTail = 0;
@@ -150,6 +203,41 @@ namespace BleScanner {
             var b = SerialBuffer[ SerialBufferTail ];
             SerialBufferTail++;
             return b;
+        }
+
+        #region Serial Protocol Helpers
+
+        static Task StartEventPollingTask( CancellationToken cancelToken ) {
+            return Task.Run( () => {
+                while ( !cancelToken.IsCancellationRequested ) {
+                    var b = ReadByteFromSerialBuffer();
+                    Protocol.Parse( b );
+                    if ( CheckForProtocolError( out var error ) ) {
+                        PrintError( $"Protocol error: {error}" );
+                    }
+                    if ( CheckForEvent( out var eventPdu ) ) {
+                        OnEventReceived( eventPdu );
+                    }
+                }
+            }, cancelToken );
+        }
+
+        static void WriteCommand( CommandPdu command ) {
+            var commandBytes = command.ToByteArray();
+            Serial.Write( commandBytes, 0, commandBytes.Length );
+        }
+
+        static CommandResponsePdu ReadResponse() {
+            while ( true ) {
+                var rxByte = ReadByteFromSerialBuffer();
+                Protocol.Parse( rxByte );
+                if ( CheckForProtocolError( out var error ) ) {
+                    throw new Exception( $"Protocol error: {error}" );
+                }
+                if ( CheckForResponse( out var responsePdu ) ) {
+                    return responsePdu;
+                }
+            }
         }
 
         static bool CheckForEvent( out EventPdu eventPdu ) {
@@ -182,62 +270,47 @@ namespace BleScanner {
             return true;
         }
 
-        static void WaitForKeypress( CancellationToken cancelToken ) {
-            try {
-                var readKeyTask = Task.Run( () => Console.ReadKey( true ), cancelToken );
-                readKeyTask.Wait( cancelToken );
-            }
-            catch ( Exception ex ) {
-                if ( !( ex is OperationCanceledException ) ) {
-                    Debug.WriteLine( ex );
-                }
-            }
+        static void OnEventReceived( EventPdu eventPdu ) {
+            if ( eventPdu.Header.EventCode != EventCode.DeviceDiscovered ) return;
+            var discoveredEvent = eventPdu.As< DeviceDiscoveredEvent >();
+            var mac = DataConverter.ByteArrayToHexString( discoveredEvent.BluetoothAddress, delimiter: ":" ); 
+            var rssi = discoveredEvent.RSSI;
+            var bleAdvRawData = DataConverter.ByteArrayToHexString( discoveredEvent.AdData, delimiter: " " );
+            // Print to console
+            Console.WriteLine( $"{mac}  {rssi:###} dBm  {bleAdvRawData}" );
+            // Forward to Loop Location Engine
+            SendBleRssiToLoop( discoveredEvent );
         }
 
-        static void ParseArgs( string[] args ) {
-            try {
-                PortName = args[ 0 ];
-                if ( args.Length > 1 ) {
-                    Baud = int.Parse( args[ 1 ] );
-                }
-            }
-            catch ( Exception ex ) {
-                Debug.WriteLine( ex );
-                PrintHelp();
-            }
+        #endregion
+
+        #region Loop Location Engine Helpers
+
+        private static void SendBleRssiToLoop( DeviceDiscoveredEvent eventPdu ) {
+            // Serialize into Loop BLE RSSI packet
+            var stream = new MemoryStream();
+            var writer = new BinaryWriter( stream );
+            writer.Write( (UInt16) 3 );                    // Packet Type: BLE RSSI
+            writer.Write( ScannerMacAddress );             // BLE Scanner MAC Address (Bluetooth Address)
+            writer.Write( eventPdu.BluetoothAddress );     // Remote Device Bluetooth Address
+            writer.Write( (byte) 0x00 );                   // *Reserved
+            writer.Write( eventPdu.RSSI );                 // RSSI
+            writer.Write( (byte) 0x00 );                   // *Reserved
+            writer.Write( SequenceNumber++ );              // Sequence Number
+            writer.Write( (byte) 0x00 );                   // *Reserved
+            writer.Write( (byte) 0x00 );                   // *Reserved
+            writer.Write( (byte) eventPdu.AdData.Length ); // Payload Length
+            writer.Write( eventPdu.AdData );               // Payload Data
+
+            var loopPacket = stream.ToArray();
+            writer.Dispose();
+            stream.Dispose();
+
+            // Send over UDP
+            LoopSocket.SendTo( loopPacket, Endpoint );
         }
 
-        static void PrintHelp() {
-            Console.WriteLine();
-            Console.WriteLine( "Usage: BleScanner serialport | -h" );
-            Console.WriteLine();
-            Console.WriteLine( "  -h, --help                 Print this message" );
-        }
-
-        static void PrintError( string message ) {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine( message );
-            Console.ResetColor();
-        }
-
-        static void Cleanup() {
-            try {
-                // Send StopScan to reset the scanner state
-                if ( Serial != null ) { 
-                    WriteCommand( SerialProtocol.CreateStopScanCommand() );
-                }
-            }
-            catch ( Exception ex ) {
-                PrintError( ex.Message );
-            }
-            try {
-                Serial?.Close();
-            }
-            catch ( Exception ex ) {
-                PrintError( ex.Message );
-            }
-        }
-
+        #endregion
     }
 
 }
